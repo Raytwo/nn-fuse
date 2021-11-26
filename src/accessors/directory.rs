bc2112cec37537bf778032278874d1f6dd504a68
Raytwo <raytwost@gmail.com>
2021-11-26 23:33:13 +0100

    (Seemingly) fix FsDirectoryAccessor trait

--- src/accessors/directory.rs
+++ src/accessors/directory.rs
@@ -20,10 +20,8 @@ impl<D: FsDirectoryAccessor> DirectoryAccessor<D> {
 #[repr(C)]
 struct DirectoryAccessorVtable<D: FsDirectoryAccessor> {
     // also type info at VTable - 0x8
-    add_reference: extern "C" fn(&mut D),
-    release: extern "C" fn(&mut D),
-    get_proxy_info: extern "C" fn(&mut D),
-    get_interface_type_info: extern "C" fn(&mut D),
+    destructor: extern "C" fn(&mut D),
+    deleter: extern "C" fn(&mut D),
     read: extern "C" fn(&mut D, &mut isize, *mut nn::fs::DirectoryEntry, usize) -> AccessorResult,
     get_entry_count: extern "C" fn(&mut D, &mut isize) -> AccessorResult
 }
@@ -31,10 +29,8 @@ struct DirectoryAccessorVtable<D: FsDirectoryAccessor> {
 impl<D: FsDirectoryAccessor> DirectoryAccessorVtable<D> {
     fn new() -> Self {
         Self {
-            add_reference: D::add_reference,
-            release: D::release,
-            get_proxy_info: D::get_proxy_info,
-            get_interface_type_info: D::get_interface_type_info,
+            destructor: D::destructor,
+            deleter: D::deleter,
             read: D::read,
             get_entry_count: D::get_entry_count,
         }
@@ -43,10 +39,11 @@ impl<D: FsDirectoryAccessor> DirectoryAccessorVtable<D> {
 
 pub trait FsDirectoryAccessor {
     // also type info at VTable - 0x8
-    extern "C" fn add_reference(&mut self);
-    extern "C" fn release(&mut self);
-    extern "C" fn get_proxy_info(&mut self);
-    extern "C" fn get_interface_type_info(&mut self);
+    extern "C" fn destructor(&mut self);
+    extern "C" fn deleter(&mut self) where Self: Sized {
+        self.destructor();
+        fs::detail::free(self as *mut Self); 
+    }
     extern "C" fn read(&mut self, out_count: &mut isize, buffer: *mut nn::fs::DirectoryEntry, buffer_len: usize) -> AccessorResult {
         AccessorResult::Unimplemented
     }
